fb3561828ce3b71368c3a2048a5bea7297a73977
listder <listder@outlook.com>
2024-04-13 02:41:54 +0800

    update

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using pbd2json.Properties;
+
+namespace pbd2json{
+	internal class Program{
+		private static void Main(string[] args){
+			try{
+				string text = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+				string fullPath = Path.GetFullPath(args[0]);
+				string text2 = Path.Combine(text, "out.json");
+				Directory.CreateDirectory(text);
+				using (ZipArchive zipArchive = new ZipArchive(new MemoryStream(Resources.pbd2json))){
+					foreach (ZipArchiveEntry zipArchiveEntry in zipArchive.Entries){
+						using (FileStream fileStream = new FileStream(Path.Combine(text, zipArchiveEntry.FullName), FileMode.Create)){
+							using (Stream stream = zipArchiveEntry.Open()){
+								stream.CopyTo(fileStream);
+							}
+						}
+					}
+				}
+				Process process = Process.Start(new ProcessStartInfo(Path.Combine(text, "tvpwin32.exe")){
+					Arguments = string.Concat(new string[]{
+						text,
+						" -in=",
+						Uri.EscapeDataString(fullPath),
+						" -out=",
+						Uri.EscapeDataString(text2)
+					}),
+					RedirectStandardError = true,
+					RedirectStandardOutput = true,
+					UseShellExecute = false
+				});
+				process.BeginOutputReadLine();
+				process.BeginErrorReadLine();
+				process.WaitForExit();
+				Console.WriteLine(File.ReadAllText(text2));
+			}
+			catch{
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("pbd2json")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pbd2json")]
+[assembly: AssemblyCopyright("Copyright © lictex_")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("aa33bbd4-0673-46f1-bd34-7976b85a69fb")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
